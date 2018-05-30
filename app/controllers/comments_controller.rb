dc624a028e62fc7eb76e15b0ce08bdf8d1157b20
shukan0728 <cielo28verano@gmail.com>
2018-05-30 11:10:09 +0900

    コメント機能実装

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,20 @@
+class CommentsController < ApplicationController
+
+  def create
+    @comment = Comment.create(contnet: comment_params[:content], prototype_id: comment_params[:comment_id]), user_id: current_user.id)
+     redirect_to prototype_path(params[:prtotype_id])
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+  def comment_params
+    params.permit(:content, :prototype_id, :user_id)
+  end
+
+
+end
