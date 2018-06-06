b80a02908feb822f9bdb0ca64250bfaae8a50198
shukan0728 <cielo28verano@gmail.com>
2018-06-06 11:49:21 +0900

    修正

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,14 +1,13 @@
 class LikesController < ApplicationController
 
   def create
-    @like = like.create(user_id: current_user.id, prototype_id: params[:params_id])
-    @likes = Like.where(prototepe_id: params[:prtotype_id])
-
+    @like = Like.create(user_id: current_user.id, prototype_id: params[:prototype_id])
+    @likes = Like.where(prototepe_id: params[:prototype_id])
   end
 
   def destroy
-    like = Like.find_by(user_id: current_user.id, prototype_id: params[:prtotype_id])
+    like = Like.find_by(user_id: current_user.id, prototype_id: params[:prototype_id])
     like.destroy
     @likes = Like.where(protoypr_id: parmas[:prototype_id])
   end
-
+end
