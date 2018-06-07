cd7525b8b924a43d0371855074de825dcb33801d
hiroaki <sanotaiyou@sanotaiyounoMacBook-puro.local>
2018-06-07 10:57:59 +0900

    コメント送信・表示機能部分の実装
    非同期およびコメントの編集・削除は未実装
    コメントのカウントについては、表示

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,8 +1,14 @@
 class CommentsController < ApplicationController
+  skip_before_filter :verify_authenticity_token
 
   def create
-    @comment = Comment.new(content: comment_params[:content], prototype_id: comment_params[:prototype_id], user_id: current_user.id)
-    redirect_to prototype_path(@prototype)
+    @comment = Comment.create(content: comment_params[:content], prototype_id: comment_params[:prototype_id], user_id: current_user.id)
+    prototype = Prototype.find(params[:prototype_id])
+    @comments = prototype.comments.includes(:user)
+    redirect_to "/prototypes/#{@comment.prototype.id}"
+  end
+
+  def show
   end
 
   def update
@@ -13,7 +19,8 @@ class CommentsController < ApplicationController
 
   private
   def comment_params
-    params.permit(:content, :prototype_id, :user_id)
+    # params.permit(:content, :prototype_id, :user_id)
+    params.require(:comment).permit(:content).merge(prototype_id: params[:prototype_id])
   end
 
 end
